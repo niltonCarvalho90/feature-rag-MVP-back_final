092579e186a6e0af12b48b25401f9ee29f1d2472
Josenilton Carvalho de Sousa <nilton.carvalho@boavista.local>
2021-12-06 15:00:31 -0300

    Atualizando

--- src/main/java/br/com/implant_rag_back/controllers/OperadoraController.java
+++ src/main/java/br/com/implant_rag_back/controllers/OperadoraController.java
@@ -8,10 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -60,9 +60,9 @@ public class OperadoraController {
 
     }
 	
-	@DeleteMapping(value = "/deletar")
+	@DeleteMapping(value = "/deletar/{id}")
 	@ResponseBody
-	public  ResponseEntity<String> remover(@RequestParam Long id) {
+	public  ResponseEntity<String> remover(@PathVariable Long id) {
 		operadoraService.removerOperadora(id);
 		
 		return new ResponseEntity<String>("Deletado com sucesso", HttpStatus.OK);
